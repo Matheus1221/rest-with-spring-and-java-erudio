31cf30e4f72cfa1a08b4bc5f98e99de4318a5aaf
Matheus Nascimento <tritanius1221@gmail.com>
2024-04-16 11:14:09 -0300

    Migrations FlyWay

---
+++ 09_migrationsFlyWay/rest-with-spring-and-java-erudio/src/main/resources/db/migration/V2__Poulate_Table_Person.sql
@@ -0,0 +1,10 @@
+INSERT INTO `person` (`id`, `address`, `gender`, `last_name`, `name`) VALUES
+	(1, 'São Paulo', 'Male', 'Quon', 'Matheus'),
+	(2, 'Anchiano - Italy', 'Male', 'Da vinci', 'Leonardo'),
+	(3, 'São Paulo - Brasil', 'Male', 'Henrique', 'João'),
+	(4, 'São Paulo - Brasil', 'Female', 'martins', 'maiyuri'),
+	(8, 'Kentucky - USA', 'Male', 'Ali', 'Muhammad'),
+	(10, 'Xangai - China', 'Male', 'Tzu', 'Sun'),
+	(11, 'Smiljan - Croatia', 'Male', 'Tesla', 'Nikola'),
+	(12, 'Smiljan - Croatia', 'Male', 'Tesla', 'Nikola'),
+	(13, 'Smiljan - Croatia', 'Male', 'Tgsdgesla', 'ffggf');
