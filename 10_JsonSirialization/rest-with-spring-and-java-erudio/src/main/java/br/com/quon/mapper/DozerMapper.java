2f1537e1e22c32d7d90920784176811203740bf8
Matheus Nascimento <tritanius1221@gmail.com>
2024-04-17 13:02:30 -0300

    JSON SIRIALIZATION

---
+++ 10_JsonSirialization/rest-with-spring-and-java-erudio/src/main/java/br/com/quon/mapper/DozerMapper.java
@@ -0,0 +1,29 @@
+package br.com.quon.mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import br.com.quon.data.vo.v1.PersonVO;
+import br.com.quon.model.Person;
+
+public class DozerMapper {
+
+	
+	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	public static <O, D> D parseObject(O origin, Class<D> destination) {
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+		List<D> destinationObjects = new ArrayList<D>();
+		for (O o : origin) {
+			destinationObjects.add(mapper.map(o, destination));
+		}
+		return destinationObjects;
+	}
+
+}
