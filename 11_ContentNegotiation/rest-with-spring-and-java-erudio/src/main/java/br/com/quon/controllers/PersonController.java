95909185e7a1c558f6e8d92268362d87d07f8997
Matheus Nascimento <tritanius1221@gmail.com>
2024-04-17 14:43:35 -0300

    mediaTypeURL

---
+++ 11_ContentNegotiation/rest-with-spring-and-java-erudio/src/main/java/br/com/quon/controllers/PersonController.java
@@ -0,0 +1,62 @@
+package br.com.quon.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import br.com.quon.data.vo.v1.PersonVO;
+
+import br.com.quon.services.PersonServices;
+
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+	
+	@Autowired
+	private PersonServices service ;
+
+	
+
+	@GetMapping(
+			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public List<PersonVO> findAll() {
+
+		return service.findAll();
+	}
+	
+	
+	@GetMapping(value = "/{id}",
+			produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public PersonVO findById(@PathVariable (value = "id") Long id) throws Exception {
+
+		return service.findById(id);
+	}
+	
+	@PostMapping(
+			consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+			produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public PersonVO create(@RequestBody PersonVO person) {
+		return service.create(person);
+	}
+	
+	@PutMapping(
+			consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+			produces= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public PersonVO update(@RequestBody PersonVO person) {
+		return service.update(person);
+	}
+
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable (value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();	}
+
+	
+	
+}
---
+++ 11_ContentNegotiation/rest-with-spring-and-java-erudio/src/main/java/br/com/quon/exception/CustomizeResponseEntityExceptionHandler.java
@@ -0,0 +1,44 @@
+package br.com.quon.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ExceptionResponse> handleAllException(
+			Exception ex, WebRequest request){
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), 
+				ex.getMessage(),
+				request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public final ResponseEntity<ExceptionResponse> handleNotFoundException(
+			Exception ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), 
+				ex.getMessage(),
+				request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+		
+	}
+
+}
